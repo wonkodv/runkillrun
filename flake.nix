2405fbf977e4d2e7b7141b9bfe5c50af024e573e
Wonko <wonko@hanstool.org>
2025-01-04 23:29:27 +0100

    flake: make it a package and app

--- flake.nix
+++ flake.nix
@@ -1,4 +1,6 @@
 {
+  description = "*Run* an executable Program. When the exe file changes *kill* the old process. *Run* the new exe again.";
+
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils?ref=c1dfcf08411b08f6b8615f7d8971a2bfa81d5e8a";
@@ -14,11 +16,9 @@
       system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-        buildDeps = with pkgs; [
+        deps = with pkgs; [
           cargo
           rustc
-        ];
-        deps = with pkgs; [
           cargo-expand
           gdb
           clippy
@@ -28,8 +28,25 @@
         ];
       in
       {
-        devShells.default = pkgs.mkShell { buildInputs = deps ++ buildDeps; };
+        packages.default = pkgs.rustPlatform.buildRustPackage {
+          pname = "rkr";
+          version = "0.1.0"; # keep in synch with Cargo.toml version
+          src = ./.;
+
+          cargoHash = "sha256-yS9jl2PX0uXWtYecmKy62DY7q/xhdYb/DkMrpkbybUk=";
+
+          meta = with pkgs.lib; {
+            description = "A simple Rust CLI application";
+            homepage = "https://github.com/wonkodv/runkillrun";
+          };
+        };
+
+        devShells.default = pkgs.mkShell { buildInputs = deps; };
+
+        apps.default = {
+          type = "app";
+          program = "${self.packages.${system}.default}/bin/rkr";
+        };
       }
-      )
-    ;
+    );
 }
